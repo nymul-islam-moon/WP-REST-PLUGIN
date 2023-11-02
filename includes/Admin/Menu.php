40c5c38a46e12d566f15866e2f2dada26c4d9298
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-11-02 12:54:28 +0600

    Video 2 at 6.56

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -12,12 +12,20 @@ class Menu {
     }
 
     public function admin_menu() {
-        add_menu_page( __( 'WP REST PLUGIN', 'wp-rest-plugin' ), __( 'WP REST PLUGIN', 'wp-rest-plugin' ), 'manage_options', 'wp-rest-plugin', [ $this, 'plugin_page' ],  'dashicons-welcome-learn-more' );
 
-//        add_submenu_page(  );
+        $parent_slug = 'wp-rest-plugin';
+        $capability = 'manage_options';
+
+        add_menu_page( __( 'WP REST PLUGIN', 'wp-rest-plugin' ), __( 'WP REST PLUGIN', 'wp-rest-plugin' ), $capability, $parent_slug, [ $this, 'plugin_page' ],  'dashicons-welcome-learn-more' );
+
+        add_submenu_page( $parent_slug, __( 'Address Book', 'wp-rest-plugin' ), __( 'Address Book', 'wp-rest-plugin' ), $capability, 'wp-rest-plugin-address-book', [ $this, 'address_book_page' ]);
     }
 
     public function plugin_page() {
         echo "hello world";
+    }
+
+    public function address_book_page() {
+        echo 'Hello Address Book';
     }
 }
