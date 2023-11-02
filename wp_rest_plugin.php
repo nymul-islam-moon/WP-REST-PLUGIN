90f4867018c7e6add4df6e66463e5b655214ff21
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-11-02 10:23:20 +0600

    Plugin main page created and main class defined

---
+++ wp_rest_plugin.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * WP-REST-PLUGIN
+ *
+ * @package           WP-REST-PLUGIN
+ * @author            Nymul Islam Moon
+ * @copyright         2023 Nymul Islam
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP-REST-PLUGIN
+ * Plugin URI:        https://github.com/nymul-islam-moon/Author-Bio
+ * Description:       It will contain author information and additional links
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Nymul Islam Moon
+ * Author URI:        https://github.com/nymul-islam-moon
+ * Text Domain:       author-bio
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://github.com/nymul-islam-moon/Author-Bio
+ */
+
+/**
+ * Copyright (c) 2023 Nymul Islam ( email: towkir1997islam@gmail.com ). All rights reserved.
+ *
+ * Released under the GPL license
+ * htp://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpres.org/
+ *
+ * *********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Geneal Public License as published by
+ * the Free Software Foundation; eiher version 2of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope hat it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Ring Road, Mohammadpur, Dhaka, Bangladesh.
+ * ********************************************************************
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * The main plugin class
+ */
+final class Wp_Rest_Plugin {
+
+    /**
+     * Class constructor
+     */
+    private function __construct() {
+
+    }
+
+    /**
+     * Initializes a singleton instance
+     *
+     * @return false|Wp_Rest_Plugin
+     */
+    public static function init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+}
