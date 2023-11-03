7ab7ed9842b4b493684258c4ad3f37a448b74b48
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-11-03 10:38:00 +0600

    class 2 at 13.00 minute

--- includes/Admin/AddressBook.php
+++ includes/Admin/AddressBook.php
@@ -15,7 +15,30 @@ class AddressBook {
     }
 
     public function plugin_page() {
-        echo "Hello from plugin page in AddressBook class";
+        $action = isset( $_GET['action'] ) ? $_GET['action'] : 'list';
+
+        switch ( $action ) {
+            case 'new' :
+                $template = __DIR__ . '/views/address-new.php';
+                break;
+
+            case 'edit' :
+                $template = __DIR__ . '/views/address-edit.php';
+                break;
+
+            case 'view' :
+                $template = __DIR__ . '/views/address-view.php';
+                break;
+
+            default :
+                $template = __DIR__ . '/views/address-list.php';
+                break;
+        }
+
+        if ( file_exists( $template ) ) {
+            include $template;
+        }
+
     }
 
 }
---
+++ includes/Admin/views/address-list.php
@@ -0,0 +1,3 @@
+<div class="wrap">
+    <h1><?php _e( 'Address Book', 'wp-rest-plugin' ); ?></h1>
+</div>
